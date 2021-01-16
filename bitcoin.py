a9dd0e7bf4eff92b9f23439dfe017b789e4c4bfa
J Francisco Rader <franciscorader@gmail.com>
2021-01-16 17:58:39 -0500

    try fix

--- bitcoin.py
+++ bitcoin.py
@@ -33,32 +33,20 @@ class Bitcoin(plugins.Plugin):
     def on_ui_setup(self, ui):
         if ui.is_waveshare_v2():
             h_pos = (180, 80)
-            v_pos = (180, 61)
         elif ui.is_waveshare_v1():
             h_pos = (170, 80)
-            v_pos = (170, 61)
         elif ui.is_waveshare144lcd():
             h_pos = (53, 77)
-            v_pos = (78, 67)
         elif ui.is_inky():
             h_pos = (140, 68)
-            v_pos = (165, 54)
         elif ui.is_waveshare27inch():
             h_pos = (192, 138)
-            v_pos = (216, 122)
         else:
             h_pos = (155, 76)
-            v_pos = (180, 61)
 
-        if self.options['orientation'] == "vertical":
-            ui.add_element('bitcoin', LabeledValue(color=BLACK, label='', value=' btc/usd:$-',
-                                                   position=v_pos,
-                                                   label_font=fonts.Small, text_font=fonts.Small))
-        else:
-            # default to horizontal
-            ui.add_element('bitcoin', LabeledValue(color=BLACK, label='', value='btc/usd:\n $-',
-                                                   position=h_pos,
-                                                   label_font=fonts.Small, text_font=fonts.Small))
+        ui.add_element('bitcoin', LabeledValue(color=BLACK, label='', value=' btc/usd:$ ...',
+                                                position=h_pos,
+                                                label_font=fonts.Small, text_font=fonts.Small))
 
     def on_unload(self, ui):
         with ui._lock:
@@ -67,9 +55,4 @@ class Bitcoin(plugins.Plugin):
     def on_internet_available(self, ui):
         prices = fetch_prices()
         current_price = prices[len(prices) - 1]
-        if self.options['orientation'] == "vertical":
-            ui.set('bitcoin', ' btc/usd:${}'.format(current_price))
-        else:
-            # default to horizontal
-            ui.set('bitcoin',
-                    ' btc/usd:\n ${} '.format(current_price))
+        ui.set('bitcoin', ' btc/usd:$ %.2f ' % (current_price))
