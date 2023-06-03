93e4922374768d2688b8398fcc5201d7fe171e1c
Bauke Molenaar <bauke.molenaar@gmail.com>
2023-06-04 01:16:23 +0200

    modified:   apprise-notify.py

--- apprise-notify.py
+++ apprise-notify.py
@@ -32,6 +32,8 @@ apobj.add(config)
 #     title='my notification title',
 # )
 
+picture = '/var/tmp/pwnagotchi/pwnagotchi.png' if os.path.exists("/var/tmp/pwnagotchi/pwnagotchi.png") else '/root/pwnagotchi.png'
+
 class Apprise(plugins.Plugin):
     __author__ = 'bauke.molenaar@gmail.com'
     __version__ = '1.0.0'
@@ -54,11 +56,10 @@ class Apprise(plugins.Plugin):
         self.text_to_set = ""
         title=("[apprise] A rare photo of a pwnagotchi.")
         body=("They are often well hidden from plain sight! but not this one, hah!")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when http://<host>:<port>/plugins/<plugin>/ is called
@@ -67,44 +68,40 @@ class Apprise(plugins.Plugin):
     def on_webhook(self, path, request):
         title=("[apprise]")
         body=("Webhook clicked! " + path + " " + request)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the plugin is loaded
     def on_loaded(self):
         title=("[apprise]")
         body=("plugin loaded")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called before the plugin is unloaded
     def on_unload(self, ui):
         title=("[apprise]")
         body=("plugin unloaded")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called hen there's internet connectivity
     def on_internet_available(self, agent):
         title=("[apprise]")
         body=("I now have internet.")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called to setup the ui elements
@@ -112,220 +109,200 @@ class Apprise(plugins.Plugin):
         # add custom UI elements
         title=("[apprise]")
         body=("Setting up UI elements")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # # called when the ui is updated
     def on_ui_update(self, ui):
         title=("[apprise]")
         body=("The UI is updated")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the hardware display setup is done, display is an hardware specific object
     def on_display_setup(self, display):
         title=("[apprise]")
         body=("plugin created")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when everything is ready and the main loop is about to start
     def on_ready(self, agent):
         title=("[apprise]")
         body=("unit is ready!")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the AI finished loading
     def on_ai_ready(self, agent):
         title=("[apprise]")
         body=("The AI is finished loading")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the AI finds a new set of parameters
     def on_ai_policy(self, agent, policy):
         title=("[apprise]")
         body=("I have found a new set of parameters. Policy: " + policy)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the AI starts training for a given number of epochs
     def on_ai_training_start(self, agent, epochs):
         title=("[apprise]")
         body=("The AI has started training. Epochs: " + epochs)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called after the AI completed a training epoch
     def on_ai_training_step(self, agent, _locals, _globals):
         title=("[apprise]")
         body=("The AI has completed training for an epoch.")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the AI has done training
     def on_ai_training_end(self, agent):
         title=("[apprise]")
         body=("The AI is done with training.")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the AI got the best reward so far
     def on_ai_best_reward(self, agent, reward):
         title=("[apprise]")
         body=("The AI just got its best reward so far. Reward: " + reward)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the AI got the worst reward so far
     def on_ai_worst_reward(self, agent, reward):
         title=("[apprise]")
         body=("The AI just got its worst reward so far. Reward: " + reward)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when a non overlapping wifi channel is found to be free
     def on_free_channel(self, agent, channel):
         title=("[apprise]")
         body=("I just found a non overlapping wifi channel: " + channel + " that is free.")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the status is set to bored
     def on_bored(self, agent):
         title=("[apprise]")
         body=("I am so bored right now...")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the status is set to sad
     def on_sad(self, agent):
         title=("[apprise]")
         body=("I am so sad...")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the status is set to excited
     def on_excited(self, agent):
         title=("[apprise]")
         body=("I am so excited...")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the status is set to lonely
     def on_lonely(self, agent):
         title=("[apprise]")
         body=("I am so loneley, nobody wants to play with me...")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent is rebooting the board
     def on_rebooting(self, agent):
         title=("[apprise]")
         body=("I am going to reboot now.")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent is waiting for t seconds
     def on_wait(self, agent, t):
         title=("[apprise]")
         body=("Waiting for " + t + " seconds...")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent is sleeping for t seconds
     def on_sleep(self, agent, t):
         title=("[apprise]")
         body=("Sleeping for " + t + " seconds ...")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent refreshed its access points list
     def on_wifi_update(self, agent, access_points):
         title=("[apprise]")
         body=("I have refreshed my list of access points. Access points: " + access_points)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent refreshed an unfiltered access point list
@@ -333,44 +310,40 @@ class Apprise(plugins.Plugin):
     def on_unfiltered_ap_list(self, agent, access_points):
         title=("[apprise]")
         body=("I have refreshed my list of unfilteted access points. Access points: " + access_points)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent is sending an association frame
     def on_association(self, agent, access_point):
         title=("[apprise]")
         body=("I am sending " + access_point + " an association frame now.")
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent is deauthenticating a client station from an AP
     def on_deauthentication(self, agent, access_point, client_station):
         title=("[apprise]")
         body=("I am deauthenticating " + client_station + "from " + access_point)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when the agent is tuning on a specific channel
     def on_channel_hop(self, agent, channel):
         title=("[apprise]")
         body=("I am running on " + channel)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when a new handshake is captured, access_point and client_station are json objects
@@ -378,52 +351,47 @@ class Apprise(plugins.Plugin):
     def on_handshake(self, agent, filename, access_point, client_station):
         title=("[apprise]")
         body=("I have captured a handshake. \nFilename: " + filename + "\nClient station: " + client_station + "\nAccess point: " + access_point)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when an epoch is over (where an epoch is a single loop of the main algorithm)
     def on_epoch(self, agent, epoch, epoch_data):
         title=("[apprise]")
         body=("I have completed a whole epoch. \nEpoch: " + epoch + "\nEpoch data: " + epoch_data)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when a new peer is detected
     def on_peer_detected(self, agent, peer):
         title=("[apprise]")
         body=("I have found a new peer. \nPeer: " + peer)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     # called when a known peer is lost
     def on_peer_lost(self, agent, peer):
         title=("[apprise]")
         body=("I have lost contact with a peer. \nPeer: " + peer)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
 
     def on_cracked(self, agent, access_point):
         title=("[apprise]")
         body=("I have cracked the password for: " + access_point)
-        attach=("/var/tmp/pwnagotchi/pwnagotchi.png")
         apobj.notify(
             title=title,
             body=body,
-            attach=attach,
+            attach=picture,
         )
