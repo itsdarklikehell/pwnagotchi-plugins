967da456bf29f3dc00446c52dfb76ea78f8cee8d
Bauke Molenaar <bauke.molenaar@gmail.com>
2023-06-03 20:19:26 +0200

    modified:   apprise-notify.py

--- apprise-notify.py
+++ apprise-notify.py
@@ -177,15 +177,15 @@ class Apprise(plugins.Plugin):
         )
 
     # called when the ui is updated
-    def on_ui_update(self, ui):
-        title=("[apprise]")
-        body=("The UI is updated")
-        logging.info(title)
-        logging.info(body)
-        apobj.notify(
-            title=title,
-            body=body,
-        )
+    # def on_ui_update(self, ui):
+    #     title=("[apprise]")
+    #     body=("The UI is updated")
+    #     logging.info(title)
+    #     logging.info(body)
+    #     apobj.notify(
+    #         title=title,
+    #         body=body,
+    #     )
 
     # called when the hardware display setup is done, display is an hardware specific object
     def on_display_setup(self, display):
