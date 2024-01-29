412038324b2a80a10a16adbbb6d393e28747f6b2
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-01-29 01:01:48 +0100

    modified:   scripts/tools-installer.sh

--- scripts/tools-installer.sh
+++ scripts/tools-installer.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 #set -e
-if [ "$HOSNAME" != "WifiKirby" ]; then
-    GOTCHI_ADDR=pi@WifiKirby
+if [ "$HOSNAME" != "Paimon" ]; then
+    GOTCHI_ADDR=pi@Paimon
 else
     GOTCHI_ADDR=localhost
 fi
 
-raspi-config() {
-    ssh "$GOTCHI_ADDR" "sudo raspi-config"
-}
-
 bettercaplets() {
     echo "Setting up caplets..."
-    ssh "$GOTCHI_ADDR" "sudo bettercap -eval 'caplets.update; ui.update; quit'"
-    ssh "$GOTCHI_ADDR" "sudo nano /usr/local/share/bettercap/caplets/pwnagotchi-auto.cap /usr/local/share/bettercap/caplets/pwnagotchi-manual.cap /usr/local/share/bettercap/caplets/http-ui.cap /usr/local/share/bettercap/caplets/https-ui.cap"
+    sudo bettercap -eval 'caplets.update; ui.update; quit'
+    sudo nano /usr/local/share/bettercap/caplets/pwnagotchi-auto.cap /usr/local/share/bettercap/caplets/pwnagotchi-manual.cap /usr/local/share/bettercap/caplets/http-ui.cap /usr/local/share/bettercap/caplets/https-ui.cap
 }
 
 update_apt() {
     echo "Updating apt..."
-    ssh "$GOTCHI_ADDR" "sudo nano /etc/apt/preferences.d/kali.pref"
-    ssh "$GOTCHI_ADDR" "wget -O - https://re4son-kernel.com/keys/http/archive-key.asc | sudo apt-key add -"
-    ssh "$GOTCHI_ADDR" "sudo apt-key adv --keyserver hkp://pgp.mit.edu --recv-keys 11764EE8AC24832F"
-    ssh "$GOTCHI_ADDR" "sudo apt update --allow-releaseinfo-change-suite"
-    ssh "$GOTCHI_ADDR" "sudo apt upgrade -y"
-    ssh "$GOTCHI_ADDR" "sudo apt full-upgrade -y"
-    ssh "$GOTCHI_ADDR" "sudo apt install --fix-missing"
-    ssh "$GOTCHI_ADDR" "sudo apt install --fix-broken"
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt full-upgrade -y
+    sudo apt install --fix-missing
+    sudo apt install --fix-broken
 }
+
 update_pwnagotchi() {
     echo "Updating pwnagotchi..."
-    ssh "$GOTCHI_ADDR" "cd /usr/local/src/pwnagotchi/ && sudo git pull && sudo pip3 install ."
+    sudo pwnagotchi --version
+    sudo pwnagotchi --check-update
 }
 
 edit_pwnlib() {
     echo "editting pwnlib..."
-    ssh "$GOTCHI_ADDR" "sudo nano /usr/bin/pwnlib"
+    sudo nano /usr/bin/pwnlib
 }
 
 edit_interfaces() {
     echo "editting interfaces..."
-    ssh "$GOTCHI_ADDR" "sudo nano /etc/network/interfaces.d/*"
+    sudo nano /etc/network/interfaces.d/*
 }
 
 install_sounds() {
@@ -50,35 +45,75 @@ install_sounds() {
     ssh "$GOTCHI_ADDR" "pico2wave -w lookdave.wav 'Look Dave, I can see youre really upset about this.' && aplay lookdave.wav"
 }
 
-install_aircrack() {
-    echo "Installing Aircrack..."
-    ssh "$GOTCHI_ADDR" "sudo apt install -y aircrack-ng nmap macchanger espeak"
-    ssh "$GOTCHI_ADDR" "sudo pip3 install pytz google-api-python-client google-auth-oauthlib SpeechRecognition pyttsx3 python-telegram-bot"
+install_tools() {
+    echo "Installing Tools..."
+    sudo apt install -y aircrack-ng nmap macchanger espeak python3-pytzdata python3-googleapi python3-google-auth-oauthlib python3-speechd
 }
 
 install_seclists() {
     echo "Installing SecLists..."
     cd ~
     ssh "$GOTCHI_ADDR" "git clone https://github.com/danielmiessler/SecLists"
-    ssh "$GOTCHI_ADDR" "ln -s /home/pi/SecLists wordlists"
+    ssh "$GOTCHI_ADDR" "ln -s /etc/pwnagotchi/wordlists"
 }
 
 dns_fix() {
     ssh "$GOTCHI_ADDR" "echo nameserver 1.1.1.1 | sudo tee -a /etc/resolv.conf"
 }
 
-reboot() {
-    echo "Rebooting..."
-    ssh "$GOTCHI_ADDR" "sudo reboot now"
+update_plugins() {
+    echo "Updating Plugins..."
+    cd ~
+    sudo pwnagotchi plugins update
+    sudo pwnagotchi plugins upgrade
 }
 
-dns_fix
-raspi-config
+disable_all_plugins() {
+    echo "Disable all Plugins..."
+    cd /usr/local/share/pwnagotchi/available-plugins
+    for i in *.py; do
+        echo "[DISABLE]: ${i%%.*}"
+        sudo pwnagotchi plugins disable ${i%%.*}
+    done
+}
+
+enable_all_plugins() {
+    echo "Enable all Plugins..."
+    cd /usr/local/share/pwnagotchi/available-plugins
+    for i in *.py; do
+        echo "[ENABLING]: ${i%%.*}"
+        sudo pwnagotchi plugins enable ${i%%.*}
+    done
+}
+
+install_all_plugins() {
+    echo "Enable all Plugins..."
+    cd /usr/local/share/pwnagotchi/available-plugins
+    for i in *.py; do
+        echo "[INSTALLING]: ${i%%.*}"
+        sudo pwnagotchi plugins install ${i%%.*}
+    done
+}
+
+uninstall_all_plugins() {
+    echo "uninstall all Plugins..."
+    cd /usr/local/share/pwnagotchi/available-plugins
+    for i in *.py; do
+        echo "[UNINSTALLING]: ${i%%.*}"
+        sudo pwnagotchi plugins uninstall ${i%%.*}
+    done
+}
+
+# dns_fix
 update_apt
 update_pwnagotchi
 # bettercaplets
-edit_pwnlib
-edit_interfaces
-install_sounds
-install_aircrack
-# reboot
+# edit_pwnlib
+# edit_interfaces
+# install_sounds
+install_tools
+update_plugins
+install_all_plugins
+disable_all_plugins
+enable_all_plugins
+# uninstall_all_plugins
