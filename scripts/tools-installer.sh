6d4b3e92dfa399e4bc8a8a3e91ac9ed57fbdcfaf
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-01-29 05:38:01 +0100

    modified:   scripts/tools-installer.sh

--- scripts/tools-installer.sh
+++ scripts/tools-installer.sh
@@ -5,7 +5,6 @@ bettercaplets() {
     sudo bettercap -eval 'caplets.update; ui.update; quit'
     sudo nano /usr/local/share/bettercap/caplets/pwnagotchi-auto.cap /usr/local/share/bettercap/caplets/pwnagotchi-manual.cap /usr/local/share/bettercap/caplets/http-ui.cap /usr/local/share/bettercap/caplets/https-ui.cap
 }
-
 update_apt() {
     echo "Updating apt..."
     sudo apt update -y
@@ -14,23 +13,19 @@ update_apt() {
     sudo apt install --fix-missing
     sudo apt install --fix-broken
 }
-
 update_pwnagotchi() {
     echo "Updating pwnagotchi..."
     sudo pwnagotchi --version
     sudo pwnagotchi --check-update
 }
-
 edit_pwnlib() {
     echo "editting pwnlib..."
     sudo nano /usr/bin/pwnlib
 }
-
 edit_interfaces() {
     echo "editting interfaces..."
     sudo nano /etc/network/interfaces.d/*
 }
-
 install_sounds() {
     echo "installing sounds/picotts..."
     wget http://archive.raspberrypi.org/debian/pool/main/s/svox/libttspico-utils_1.0+git20130326-3+rpi1_armhf.deb
@@ -38,19 +33,16 @@ install_sounds() {
     sudo apt install -y ./libttspico0_1.0+git20130326-3+rpi1_armhf.deb ./libttspico-utils_1.0+git20130326-3+rpi1_armhf.deb
     pico2wave -w lookdave.wav 'Look Dave, I can see youre really upset about this.' && aplay lookdave.wav
 }
-
 install_tools() {
     echo "Installing Tools..."
     sudo apt install -y aircrack-ng nmap macchanger espeak python3-pytzdata python3-googleapi python3-google-auth-oauthlib python3-speechd sudo apt install python3-buttonshim python3-nmea2
 }
-
 install_seclists() {
     echo "Installing SecLists..."
     cd ~
     git clone https://github.com/danielmiessler/SecLists
     ln -s /etc/pwnagotchi/wordlists
 }
-
 dns_fix() {
     echo nameserver 1.1.1.1 | sudo tee -a /etc/resolv.conf
 }
@@ -94,7 +86,7 @@ uninstall_all_plugins() {
     cd /usr/local/share/pwnagotchi/available-plugins
     for i in *.py; do
         echo "[UNINSTALLING]: ${i%%.*}"
-        sudo pwnagotchi plugins uninstall ${i%%.*}
+        ssh -t pi@Paimon.local "sudo pwnagotchi plugins uninstall ${i%%.*}"
     done
 }
 
