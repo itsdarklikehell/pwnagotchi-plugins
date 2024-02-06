cfad3618bc13c2d650220953438a5b249fdf607d
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-02-06 08:21:08 +0100

    modified:   pwnmenucmd.py

--- pwnmenucmd.py
+++ pwnmenucmd.py
@@ -4,24 +4,24 @@ import sys
 # Uncomment following 2 lines if using an active (self-oscillating) buzzer.
 # Navigating the menu can be slow on an epaper display, so it helps to get
 # feedback early when changing menu items.
-#import RPi.GPIO as GPIO
-#from time import sleep
+# import RPi.GPIO as GPIO
+# from time import sleep
 
 # If using an active buzzer, change the following line to match your setup.
 # eg. "buzzerpin = 5" for a buzzer attached to GPIO #5
 # Setting this to zero will disable the buzzer.
 buzzerpin = 0
 
-if (len(sys.argv) != 2):
-    logging.info('no menu command given. Try up/down/ok/back/close/stop')
-    #print('no menu command given. Try up/down/ok/back/close/stop')
+if len(sys.argv) != 2:
+    logging.info("no menu command given. Try up/down/ok/back/close/stop")
+    print("no menu command given. Try up/down/ok/back/close/stop")
     sys.exit()
 
 cmd = sys.argv[1]
-print 'cmd = ' + cmd
-if cmd not in ('up', 'down', 'ok', 'back', 'close', 'stop'):
-    logging.info('invalid menu command given. Try up/down/ok/back/close/stop')
-    #print('invalid menu command given. Try up/down/ok/back/close/stop')
+print("cmd = " + cmd)
+if cmd not in ("up", "down", "ok", "back", "close", "stop"):
+    logging.info("invalid menu command given. Try up/down/ok/back/close/stop")
+    print("invalid menu command given. Try up/down/ok/back/close/stop")
     sys.exit()
 
 if buzzerpin:
@@ -31,9 +31,9 @@ if buzzerpin:
     GPIO.output(buzzerpin, GPIO.HIGH)
     sleep(0.025)
     GPIO.output(buzzerpin, GPIO.LOW)
-    GPIO.cleanup() # Free up GPIO resources
+    GPIO.cleanup()  # Free up GPIO resources
 
-address = ('localhost', 6789)
+address = ("localhost", 6789)
 conn = Client(address)
 conn.send(cmd)
 conn.close()
