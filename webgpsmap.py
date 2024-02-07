2ab55faa90e4a7909288e9e2f63bc2b85c040422
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-02-07 18:22:23 +0100

    modified:   webgpsmap.py

--- webgpsmap.py
+++ webgpsmap.py
@@ -29,7 +29,6 @@ class Webgpsmap(plugins.Plugin):
     __description__ = 'a plugin for pwnagotchi that shows a openstreetmap with positions of ap-handshakes in your webbrowser'
     __name__ = 'webgpsmap'
     __help__ = "A plugin that will add age and strength stats based on epochs and trained epochs"
-    )
     __dependencies__ = {
         'pip': ['flask']
     }
@@ -49,15 +48,15 @@ class Webgpsmap(plugins.Plugin):
         self.ready = True
 
     def on_loaded(self):
-"""
-        Plugin got loaded
-"""
+        """
+                Plugin got loaded
+        """
         logging.info("[webgpsmap] plugin loaded")
 
     def on_webhook(self, path, request):
-"""
-        Returns ewquested data
-"""
+        """
+                Returns ewquested data
+        """
         # defaults:
         response_header_contenttype = None
         response_header_contentdisposition = None
@@ -159,7 +158,7 @@ class Webgpsmap(plugins.Plugin):
         return PositionFile(path)
 
     def load_gps_from_dir(self, gpsdir, newest_only=False):
-"""
+        """
         Parses the gps-data from disk
         """
 
@@ -256,9 +255,9 @@ class Webgpsmap(plugins.Plugin):
         return gps_data
 
     def get_html(self):
-"""
-        Returns the html page
-"""
+        """
+                Returns the html page
+        """
         try:
             template_file = os.path.dirname(os.path.realpath(__file__)) + "/" + "webgpsmap.html"
             html_data = open(template_file, "r").read()
@@ -268,8 +267,7 @@ class Webgpsmap(plugins.Plugin):
 
 
 class PositionFile:
-    Wraps gps / net-pos files"
-    )
+    Wraps gps / net-pos files
     GPS = 1
     GEO = 2
     PAWGPS = 3
@@ -286,9 +284,9 @@ class PositionFile:
             raise js_e
 
     def mac(self):
-"""
-        Returns the mac from filename
-"""
+        """
+                Returns the mac from filename
+        """
         parsed_mac = re.search(r'.*_?([a-zA-Z0-9]{12})\.(?:gps|geo|paw-gps)\.json', self._filename)
         if parsed_mac:
             mac = parsed_mac.groups()[0]
@@ -296,31 +294,31 @@ class PositionFile:
         return None
 
     def ssid(self):
-"""
-        Returns the ssid from filename
-"""
+        """
+                Returns the ssid from filename
+        """
         parsed_ssid = re.search(r'(.+)_[a-zA-Z0-9]{12}\.(?:gps|geo|paw-gps)\.json', self._filename)
         if parsed_ssid:
             return parsed_ssid.groups()[0]
         return None
 
     def json(self):
-"""
-        returns the parsed json
-"""
+        """
+                returns the parsed json
+        """
         return self._json
 
     def timestamp_first(self):
-"""
-        returns the timestamp of AP first seen
-"""
+        """
+                returns the timestamp of AP first seen
+        """
         # use file timestamp creation time of the pcap file
         return int("%.0f" % os.path.getctime(self._file))
 
     def timestamp_last(self):
-"""
-        returns the timestamp of AP last seen
-"""
+        """
+                returns the timestamp of AP last seen
+        """
         return_ts = None
         if 'ts' in self._json:
             return_ts = self._json['ts']
@@ -334,9 +332,9 @@ class PositionFile:
         return return_ts
 
     def password(self):
-"""
-        returns the password from file.pcap.cracked or None
-"""
+        """
+                returns the password from file.pcap.cracked or None
+        """
         return_pass = None
         # 2do: make better filename split/remove extension because this one has problems with "." in path
         base_filename, ext1, ext2 = re.split('\.', self._file)
@@ -354,9 +352,9 @@ class PositionFile:
         return return_pass
 
     def type(self):
-"""
-        returns the type of the file
-"""
+        """
+                returns the type of the file
+        """
         if self._file.endswith('.gps.json'):
             return PositionFile.GPS
         if self._file.endswith('.geo.json'):
