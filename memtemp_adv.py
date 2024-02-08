5ea1de77851baf86277dfb84c779ca75368277b9
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-02-08 07:39:42 +0100

    modified:   memtemp_adv.py 	new file:   mqtt_plugin.py

--- memtemp_adv.py
+++ memtemp_adv.py
@@ -1,3 +1,30 @@
+"""
+# Plugin Setup
+1. If you have not set up a directory for custom plugins, create the directory and add its path to your config.toml.
+`main.custom_plugins = "/usr/local/share/pwnagotchi/custom-plugins/"`
+2. Install dependencies
+3. Put plugin to your custom folder
+4. Edit your config.toml
+5. Restart pwnagotchi service
+
+# Memtemp_adv Plugin
+ - Displays CPU, RAM, disk usage, CPU temperature. Support waveshare v3  
+ - Tested on 1.5.5 official version.
+
+# Dependencies
+
+```
+sudo pip3 install psutil==5.4.7
+```
+
+# Config.toml
+```
+main.plugins.memtemp_adv.enabled = true
+main.plugins.memtemp_adv.scale = "celsius"
+main.plugins.memtemp_adv.orientation = "horizontal"
+```
+"""
+
 from pwnagotchi.ui.components import LabeledValue, Text
 from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
@@ -19,7 +46,7 @@ class MemTempAdv(plugins.Plugin):
     __help__ = "A plugin that will display memory/cpu/disk usage and temperature"
     __dependencies__ = {
         "apt": ["none"],
-        "pip": ["scapy"],
+        "pip": ["scapy", "psutil"],
     }
     __defaults__ = {
         "enabled": False,
