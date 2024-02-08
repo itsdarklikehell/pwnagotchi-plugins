94b1fcbafa05573918e3fc3e4119eb3be3551feb
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-02-08 07:13:14 +0100

    deleted:    fancygotchi/img/128x128-b.png 	deleted:    fancygotchi/img/240x240-b.png 	deleted:    fancygotchi/img/240x240e.png 	deleted:    fancygotchi/img/320x240e-f.png 	deleted:    fancygotchi/img/320x240e-i.png 	deleted:    fancygotchi/img/320x240e.png 	deleted:    fancygotchi/img/480x320-b.png 	deleted:    fancygotchi/img/480x320e-i.png 	deleted:    fancygotchi/img/icons/README.md 	deleted:    fancygotchi/img/icons/favicon-bg.png 	deleted:    fancygotchi/img/icons/favicon-g.png 	deleted:    fancygotchi/img/icons/favicon-w.png 	deleted:    fancygotchi/img/icons/favicon.png 	deleted:    fancygotchi/img/ui-bg-b.png 	deleted:    fancygotchi/img/ui-bg-oled.png 	deleted:    fancygotchi/img/ui-bg-v2.png 	deleted:    fancygotchi/img/ui-bg-w-s-1.png 	deleted:    fancygotchi/img/ui-bg-w-s.png 	deleted:    fancygotchi/img/ui-bg-w.png 	deleted:    fancygotchi/img/ui-bg-w27.png 	deleted:    fancygotchi/mod/files.csv 	deleted:    fancygotchi/mod/index.html 	deleted:    fancygotchi/mod/plugins/__init__.py 	deleted:    fancygotchi/mod/plugins/default/logtail.py 	deleted:    fancygotchi/mod/ui/components.py 	deleted:    fancygotchi/mod/ui/fonts.py 	deleted:    fancygotchi/mod/ui/hw/libs/waveshare/lcdhat144/epd.py 	deleted:    fancygotchi/mod/ui/view.py 	deleted:    fancygotchi/mod/ui/web/__init__.py 	deleted:    fancygotchi/mod/ui/web/static/css/style.css 	deleted:    fancygotchi/mod/ui/web/templates/base.html 	deleted:    fancygotchi/mod/ui/web/templates/plugins.html 	deleted:    fancygotchi/mod/ui/web/templates/profile.html 	deleted:    fancygotchi/mod/usr/local/bin/pwnagotchi 	deleted:    fancygotchi/themes/.default/displayhatmini/config.toml 	deleted:    fancygotchi/themes/.default/lcdhat/config.toml 	deleted:    fancygotchi/themes/.default/spotpear24inch/config.toml 	deleted:    fancygotchi/themes/.default/style.css 	deleted:    fancygotchi/themes/.default/waveshare144lcd/config.toml 	deleted:    fancygotchi/themes/.default/waveshare27inch/config.toml 	deleted:    fancygotchi/themes/.default/waveshare35lcd/config.toml 	deleted:    fancygotchi/themes/.default/waveshare_2/config.toml 	deleted:    fancygotchi/themes/actual/README.md 	deleted:    fancygotchi/themes/hacker/lcdhat/config.toml 	new file:   httpserver.py

---
+++ httpserver.py
@@ -0,0 +1,67 @@
+import logging
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+import pwnagotchi.plugins as plugins
+from pwnagotchi.ui.components import LabeledValue
+from pwnagotchi.ui.view import BLACK
+import pwnagotchi.ui.fonts as fonts
+
+
+class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="/root/handshakes", **kwargs)
+
+
+class HttpServerPlugin(plugins.Plugin):
+    __author__ = "Hades, SgtStroopwafel"
+    __version__ = "1.0.0"
+    __license__ = "GPL3"
+    __description__ = "HTTP Server Plugin"
+    __name__ = "HttpServerPlugin"
+    __help__ = "HTTP Server Plugin"
+    __dependencies__ = {
+        "apt": ["none"],
+        "pip": ["scapy"],
+    }
+    __defaults__ = {
+        "enabled": False,
+    }
+
+    def __init__(self):
+        logging.info(f"[{self.__class__.__name__}] plugin init")
+
+    def on_loaded(self):
+        logging.info(f"[{self.__class__.__name__}] Loaded")
+        self.start_http_server()
+
+    def on_unload(self, ui):
+        self.stop_http_server()
+
+    def start_http_server(self):
+        try:
+            server_address = ("", 8000)
+            self.httpd = HTTPServer(server_address, MyHTTPRequestHandler)
+            logging.info(
+                f"[{self.__class__.__name__}] Starting HTTP server on port 8000"
+            )
+            self.httpd.serve_forever()
+
+        except Exception as e:
+            logging.error(
+                f"[{self.__class__.__name__}] Error starting HTTP server: {e}"
+            )
+
+    def stop_http_server(self):
+        try:
+            if hasattr(self, "httpd"):
+                logging.error(f"[{self.__class__.__name__}] Shutting Down HTTP Server.")
+                self.httpd.shutdown()
+
+        except Exception as e:
+            logging.error(
+                f"[{self.__class__.__name__}] Error stopping HTTP server: {e}"
+            )
+
+
+# Instantiate the plugin
+http_server_plugin = HttpServerPlugin()
