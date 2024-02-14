52fcd70222c1cdf9925b8b2f82e119bb8aed945e
Bauke Molenaar <bauke.molenaar@gmail.com>
2024-02-14 20:50:41 +0100

    modified:   httpserver.py 	modified:   wardriver.py 	modified:   wof.py

--- httpserver.py
+++ httpserver.py
@@ -1,8 +1,3 @@
-"""Edit the config.toml
-
-main.plugins.httpserver.enabled = true
-"""
-
 import logging
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
@@ -11,67 +6,41 @@ from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 
-
 class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory="/root/handshakes", **kwargs)
-
+    def __init__(self):
+        super().__init__(directory="/root/handshakes")
 
 class HttpServerPlugin(plugins.Plugin):
-    __author__ = "Hades, (edited by: itsdarklikehell bauke.molenaar@gmail.com)"
-    __version__ = "1.0.0"
-    __license__ = "GPL3"
-    __description__ = "HTTP Server Plugin"
-    __name__ = "HttpServerPlugin"
-    __help__ = "HTTP Server Plugin"
-    __dependencies__ = {
-        "apt": ["none"],
-        "pip": ["scapy"],
-    }
-    __defaults__ = {
-        "enabled": False,
-    }
-
-    def __init__(self):
-        logging.debug(f"[{self.__class__.__name__}] plugin init")
+    __author__ = 'Hades'
+    __version__ = '1.0.0'
+    __license__ = 'GPL3'
+    __description__ = 'HTTP Server Plugin'
 
     def on_loaded(self):
-        logging.info(f"[{self.__class__.__name__}] plugin loaded")
+        logging.info("[HttpServerPlugin] Loaded")
         self.start_http_server()
 
     def on_unload(self, ui):
-        logging.info(f"[{self.__class__.__name__}] plugin unloaded")
         self.stop_http_server()
 
     def start_http_server(self):
         try:
-            server_address = ("", 8000)
+            server_address = ('', 8000)
             self.httpd = HTTPServer(server_address, MyHTTPRequestHandler)
-            logging.info(
-                f"[{self.__class__.__name__}] Starting HTTP server on port 8000"
-            )
+            logging.info("[HttpServerPlugin] Starting HTTP server on port 8000")
             self.httpd.serve_forever()
 
         except Exception as e:
-            logging.error(
-                f"[{self.__class__.__name__}] Error starting HTTP server: {e}"
-            )
+            logging.error(f"[HttpServerPlugin] Error starting HTTP server: {e}")
 
     def stop_http_server(self):
         try:
-            if hasattr(self, "httpd"):
-                logging.error(
-                    f"[{self.__class__.__name__}] Shutting Down HTTP Server.")
+            if hasattr(self, 'httpd'):
+                logging.error("[HttpServerPlugin] Shutting Down HTTP Server.")
                 self.httpd.shutdown()
 
         except Exception as e:
-            logging.error(
-                f"[{self.__class__.__name__}] Error stopping HTTP server: {e}"
-            )
-
-    def on_webhook(self, path, request):
-        logging.info(f"[{self.__class__.__name__}] webhook pressed")
-
+            logging.error(f"[HttpServerPlugin] Error stopping HTTP server: {e}")
 
 # Instantiate the plugin
 http_server_plugin = HttpServerPlugin()
